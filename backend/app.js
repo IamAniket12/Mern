a4aa34b8510c02be65610375f29104ec0aed45e0
IamCoderAniket <34195159+IamCoderAniket@users.noreply.github.com>
2022-03-08 23:11:47 +0000

    env file added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose")
 const express = require("express")
+require('dotenv').config()
 const app = express()
 
-mongoose.connect('mongodb://127.0.0.1:27017/tshirt',{ useUnifiedTopology: true,useUnifiedTopology:true,useCreateIndex:true })
+mongoose.connect(process.env.DATABASE,{ useUnifiedTopology: true,useUnifiedTopology:true,useCreateIndex:true })
 .then(()=>{
     console.log("DB Connected")
 })
 
-const port = 3000
+const port = process.env.PORT||3000
 app.listen(port,()=>{
     console.log(`App is running at ${port}`)
 })
