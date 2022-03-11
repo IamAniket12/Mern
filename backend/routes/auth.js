5325fc1efd13497017dd80f0848d8d54c218a4d3
IamCoderAniket <34195159+IamCoderAniket@users.noreply.github.com>
2022-03-11 21:22:48 +0000

    Validators added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,15 @@
 var express = require("express")
 var router = express.Router()
 const {signout,signup} = require("../controllers/auth")
+const {check} = require("express-validator")
 
 router.get("/signout",signout)
 
-router.post("/signup",signup)
+router.post("/signup",[
+    check("name").isLength({min:3}).withMessage("Name should be atleast 3 Length"),
+    check("email").isEmail().withMessage("Please Enter Correct Email"),
+    check("password").isLength({min:3}).withMessage("Please enter password more that 3 lengths")
+],
+signup)
 
 module.exports = router;
